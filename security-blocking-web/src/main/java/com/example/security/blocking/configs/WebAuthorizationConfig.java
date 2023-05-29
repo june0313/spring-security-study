ae6f2db91cfc29b5ef8b585919faa045ba289c9f
ParkYoungJun <june0313@gmail.com>
2023-05-29 17:41:19 +0900

    AuthenticationEntryPoint

--- security-blocking-web/src/main/java/com/example/security/blocking/configs/WebAuthorizationConfig.java
+++ security-blocking-web/src/main/java/com/example/security/blocking/configs/WebAuthorizationConfig.java
@@ -1,8 +1,8 @@
 package com.example.security.blocking.configs;
 
+import com.example.security.blocking.services.CustomEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,9 +11,11 @@ public class WebAuthorizationConfig {
     @Bean
     public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
         return http
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(c -> {
+                    c.realmName("OTHER");
+                    c.authenticationEntryPoint(new CustomEntryPoint());
+                })
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .build();
-
     }
 }
