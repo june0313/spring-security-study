074d78c9ebc718cda8db6a15d77de6c0eeb161d0
ParkYoungJun <june0313@gmail.com>
2023-06-04 13:30:22 +0900

    ch7 - access configuration - authorities, roles

---
+++ security-ch7-app/src/main/java/com/june/ss/SecurityCh7App.java
@@ -0,0 +1,11 @@
+package com.june.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCh7App {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityCh7App.class);
+    }
+}
