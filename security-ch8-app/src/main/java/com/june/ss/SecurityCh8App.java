e5247618a53bdfaca983b04c0e1d73350eddaefd
ParkYoungJun <june0313@gmail.com>
2023-07-02 17:38:32 +0900

    ch8 - requsetMatchers

---
+++ security-ch8-app/src/main/java/com/june/ss/SecurityCh8App.java
@@ -0,0 +1,11 @@
+package com.june.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCh8App {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityCh8App.class);
+    }
+}
